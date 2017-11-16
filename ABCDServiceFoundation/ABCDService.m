94cf05ac6a82015cb88d73b520351dd1292c0001
Shakhzod Ikromov <aabbcc.double@gmail.com>
2017-11-16 18:20:42 +0300

    Added license

--- ABCDServiceFoundation/ABCDService.m
+++ ABCDServiceFoundation/ABCDService.m
@@ -1,3 +1,27 @@
+/*
+ MIT License
+ 
+ Copyright (c) 2017 Shakhzod Ikromov (GRiMe2D) <aabbcc.double@gmail.com>
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
 #import "ABCDServiceManager.h"
 #import "ABCDService.h"
 #import "ABCDService+Private.h"
@@ -16,11 +40,11 @@
 }
 
 - (BOOL)shouldFinish {
-    return [self.currentThread isCancelled];
+    return [self.thread isCancelled];
 }
 
 - (void)private_setCurrentThread:(NSThread *)thread {
-        _currentThread = thread;
+        _thread = thread;
 }
 
 - (void)private_setServiceManager:(ABCDServiceManager *)serviceManager {
@@ -34,7 +58,7 @@
 }
 
 - (void)private_finish {
-    [self.currentThread cancel];
+    [self.thread cancel];
 }
 
 - (void)start {
